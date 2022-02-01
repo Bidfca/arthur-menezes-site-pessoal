d0dc08ab68cc925c3be8a1f3103a09f124757892
arthur-menezes202 <arthurmcorrea282@gmail.com>
2022-02-01 17:51:36 -0300

    ontroduzindo informaçoes de nome e descriçao a home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,15 +5,18 @@ import React from 'react';
 function HomePage() {
   return (
     <article className="articleHomePage">
-      <hr className="lineHorizontalHomePage" />
+
       <hr className="imageHomePage" />
-      <p className="developerPresentation">Apresentanmos o Arthur Menezes Corrêa</p>
+      <p className="developerPresentation">Apresentamos o Arthur Menezes Corrêa</p>
       <p className="description">
         Bem-vindo ao meu portefólio. Convido você a explorar o meu site para entender as minhas especialidades, 
         técnicas e experiência profissional. Abaixo você encontrará uma mistura de trabalhos profissionais e de 
         alguns projetos que fazem parte da minha paixão pessoal. Divirta-se navegando e entre em contato se tiver 
         alguma pergunta.
       </p>
+      <section className="">
+
+      </section>
     </article>
   );
 }
