bea283c3fa2af2fb2ee153c71e0b27afdd992fd8
arthur-menezes202 <arthurmcorrea282@gmail.com>
2022-02-01 14:44:09 -0300

    configuraçao da home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,20 @@
+import "./homePage.css"
+
 import React from 'react';
 
 function HomePage() {
   return (
-    <div>
-      HomePage
-    </div>
+    <article className="articleHomePage">
+      <hr className="lineHorizontalHomePage" />
+      <hr className="imageHomePage" />
+      <p className="developerPresentation">Apresentanmos o Arthur Menezes Corrêa</p>
+      <p className="description">
+        Bem-vindo ao meu portefólio. Convido você a explorar o meu site para entender as minhas especialidades, 
+        técnicas e experiência profissional. Abaixo você encontrará uma mistura de trabalhos profissionais e de 
+        alguns projetos que fazem parte da minha paixão pessoal. Divirta-se navegando e entre em contato se tiver 
+        alguma pergunta.
+      </p>
+    </article>
   );
 }
 
