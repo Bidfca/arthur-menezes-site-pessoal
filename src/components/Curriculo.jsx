a7c66405a9ddd0c4ccd58a65a6870451505c23a3
arthur-menezes202 <arthurmcorrea282@gmail.com>
2022-02-01 13:37:12 -0300

    colocando css na pagina

--- src/components/Curriculo.jsx
+++ src/components/Curriculo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Curriculo() {
   return (
     <div>
-      about
+      curriculo
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,31 +7,40 @@ function Header() {
   return (
     <div className="">
       <p className="name">Arthur Menezes Correa</p>
+      <hr className="lineHorizontal" />
       <nav className="headerNavegate">
+        <Link to="/homePage">
         <button
           className="buttonNavegate"
           type="button"
         >
-          <Link to="/homePage">Home Page</Link>
+          Home Page
         </button>
+        </Link>
+        <Link to="/about">
         <button
           className="buttonNavegate"
           type="button"
         >
-          <Link to="/about">Sobre</Link>
+          Sobre
         </button>
+        </Link>
+        <Link to="/contact">
         <button
           className="buttonNavegate"
           type="button"
         >
-          <Link to="/contact">constato</Link>
+          constato
         </button>
+        </Link>
+        <Link to="/curriculo">
         <button
           className="buttonNavegate"
           type="button"
         >
-          <Link to="/curriculo">Curriculo</Link>
+          Curriculo
         </button>
+        </Link>
       </nav>
     </div>
   );
