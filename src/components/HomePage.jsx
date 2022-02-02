900d5f8f68b44008b5badc1474ec4527ec712ece
arthur-menezes202 <arthurmcorrea282@gmail.com>
2022-02-02 14:28:37 -0300

    ajustando a frase sobre falhas

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 function HomePage() {
   return (
-    <article className="articleHomePage">
-
+    <article className="">
+      <section className="sectionInfo">
       <hr className="imageHomePage" />
       <p className="developerPresentation">Apresentamos o Arthur Menezes Corrêa</p>
       <p className="description">
@@ -14,8 +14,18 @@ function HomePage() {
         alguns projetos que fazem parte da minha paixão pessoal. Divirta-se navegando e entre em contato se tiver 
         alguma pergunta.
       </p>
-      <section className="">
-
+      </section>
+      <section className="sectionBackgroundWhit">
+        <div>
+          <img 
+            src="https://www.refletirpararefletir.com.br/wp-content/uploads/2015/09/benja.jpg" 
+            className="imgBenjaminFranklin"
+            alt="benjamin franklin"
+          />
+        </div>
+        <div>
+          <p className="fraseBenjaminFranklin">Viver é enfrentar um problema atrás do outro. O modo como você o encara é que faz a diferença.</p>
+        </div>
       </section>
     </article>
   );
