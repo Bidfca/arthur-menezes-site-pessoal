0516a5718196eb496796532ab55b59f0dad62315
arthur-menezes202 <arthurmcorrea282@gmail.com>
2022-02-02 18:14:03 -0300

    criando as imagens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,24 @@
+import '../css/footer.css'
+
 import React from 'react';
 
 function Footer() {
   return (
-    <div>
-      Footer
+    <div className="footerAlign">
+      <a href="https://www.linkedin.com/in/arthur-m-correa/">
+        <img 
+            src="https://images.vexels.com/media/users/3/131245/isolated/preview/9351735cd3a456e88ad876588ab9e503-logotipo-do-linkedin.png" 
+            className="imageLinkedin"
+            alt="linkedin de Arthur Menezes"
+        />
+      </a>
+      <a href="https://www.instagram.com/zebirote/">
+      <img 
+            src="https://i1.wp.com/www.multarte.com.br/wp-content/uploads/2019/03/logo-instagram-png-fundo-transparente8.png?resize=696%2C696&ssl=1" 
+            className="imageLinkedin"
+            alt="instagram de arte pixelada"
+        />
+      </a>
     </div>
   );
 }
